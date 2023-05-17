57b5362f8c2f23115446c31373dff3b471b2d9c0
Erick Enriquez <enriquezerick182@gmail.com>
2023-05-16 19:25:44 -0700

    Delete extra popup.

--- popup/popup.js
+++ popup/popup.js
@@ -1,11 +1,15 @@
 document.getElementById('execute').addEventListener('click', () => {
   chrome.tabs.query({}, function(tabs) {
+    const tabsList = document.getElementById('tabsList');
+    tabsList.innerHTML = ''; // Clear the list before adding tabs.
+    
     for(let i = 0; i < tabs.length; i++) {
-      console.log(tabs[i].url);
+      const listItem = document.createElement('li');
+      listItem.textContent = tabs[i].url;
+      tabsList.appendChild(listItem);
     }
   });
 });
-
 
 
 
