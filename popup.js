f07aab669873231eb8e4129a4b5b39709b5dc10f
Erick Enriquez <enriquezerick182@gmail.com>
2023-05-16 20:05:03 -0700

    Show the tabs straightaway.

---
+++ popup.js
@@ -0,0 +1,10 @@
+chrome.tabs.query({}, function(tabs) {
+  const tabsList = document.getElementById('tabsList');
+  tabsList.innerHTML = ''; // Clear the list before adding tabs.
+  
+  for(let i = 0; i < tabs.length; i++) {
+    const listItem = document.createElement('li');
+    listItem.textContent = tabs[i].url;
+    tabsList.appendChild(listItem);
+  }
+});
